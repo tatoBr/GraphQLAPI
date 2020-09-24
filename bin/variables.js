660532c0249748f81118c1ddf21f9736c269168f
tatoBr <tato.l.santos@gmail.com>
2020-09-24 16:50:02 -0300

    add db related variables into variables.js file

--- bin/variables.js
+++ bin/variables.js
@@ -1,5 +1,20 @@
 exports.variables = {
-    serverVars :{
+    databaseVars: {
+        uri: `mongodb+srv://${ process.env.MONGO_USER }:${ process.env.MONGO_PASSWORD }@aifooddb.ghkbc.gcp.mongodb.net/${ process.env.MONGO_DBNAME }?retryWrites=true&w=majority`,
+        models: {
+            event: {
+                label: "Event",
+                fields: {
+                    _id: '_id',
+                    title: 'title',
+                    description: 'description',
+                    price: 'price',
+                    date: 'date'
+                }
+            }
+        }
+    },
+    serverVars: {
         PORT: 3000
     }
 }
