28011857594f4606f2600f1a47add5bf4ed850a6
tatoBr <tato.l.santos@gmail.com>
2020-09-24 16:54:12 -0300

    setup a connection to the MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+require('dotenv').config();
+
 const server = require('./bin/app');
-let { variables : { serverVars : { PORT } }} = require( './bin/variables' );
+const mongoose = require('mongoose');
+
+let { variables: { serverVars: { PORT } }, variables: { databaseVars } } = require('./bin/variables');
+
+//connect into the database
+mongoose.connect( databaseVars.uri, { useNewUrlParser: true, useUnifiedTopology: true },
+    err => {
+        if ( err ) {
+            console.log('Unable to connect to the database')
+            console.error(err.message, err);
+        }
+        else {
+            console.log('Connected to mongoDb.')
+        }
+    });
 
 const listener = server.listen(
-    PORT || process.env.PORT,
-    () => console.log( `Server running at port ${ listener.address().port }`
-));
+    process.env.PORT || PORT,
+    () => console.log(`Server running at port ${listener.address().port}`
+    ));
